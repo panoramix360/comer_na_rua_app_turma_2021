6e169c4fd85cf8fc2b475e5a58ba052ecbfbb8a7
lucas.reis <lucasreis3000@gmail.com>
2021-07-29 18:59:50 -0300

    Initial Commit

---
+++ Comer na Rua/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Comer na Rua
+//
+//  Created by Lucas Oliveira on 29/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
