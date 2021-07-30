630b2cee93023ac329b619b497cba074e2eb644b
lucas.reis <lucasreis3000@gmail.com>
2021-07-29 22:11:48 -0300

    Finalizado o esqueleto do app

---
+++ Comer na Rua/Restaurant/Controllers/RestaurantListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RestaurantListViewController.swift
+//  Comer na Rua
+//
+//  Created by Lucas Oliveira on 29/07/21.
+//
+
+import UIKit
+
+class RestaurantListViewController: UIViewController {
+    
+    @IBOutlet var restaurantListCollectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+extension RestaurantListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        collectionView.dequeueReusableCell(withReuseIdentifier: "restaurantCell", for: indexPath)
+    }
+}
